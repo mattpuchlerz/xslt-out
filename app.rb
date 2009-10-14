81b4e445df03d6a71524fb79d86ac186a3a74692
Matt Puchlerz <matt@puchlerz.com>
2009-10-14 18:17:52 -0500

    Got the app working more or less.

--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
 require 'rubygems'
 require 'sinatra'
+require 'open-uri'
 require 'xml/xslt'
+require 'htmlentities'
 
 use_in_file_templates!
 
+before do
+  @errors = []
+end
+
 get '/' do
+  erb :index
+end
+
+post '/' do
+  open(params['xml'])  { |file| @xml  = file.read } rescue @errors << 'URI given for the XML was invalid.'
+  open(params['xslt']) { |file| @xslt = file.read } rescue @errors << 'URI given for the XSLT was invalid.'
+  
+  @errors << 'Could not read the XML file.'  if not @xml
+  @errors << 'Could not read the XSLT file.' if not @xslt
+
+  if @errors.empty?
+    xslt = XML::XSLT.new
+    xslt.xml = @xml
+    xslt.xsl = @xslt
+    html = HTMLEntities.new
+    @out = html.encode xslt.serve#, :named
+  end
+  
   erb :index
 end
 
@@ -21,10 +45,11 @@ __END__
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-  	<title>untitled</title>
+  	<title>XSLT Out</title>
   	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   </head>
   <body>
+    <h1>XSLT Out</h1>
     <%= yield %>
   </body>
 </html>
@@ -33,4 +58,28 @@ __END__
 
 @@ index
 
-<h1>XSLT Out</h1>
+<form action="/" method="post">
+  <% %w[ xml xslt ].each do |field| %>
+  <dl>
+    <dt><label for="<%= field %>"><%= field.upcase %></label></dt>
+    <dd><input type="text" name="<%= field %>" value="<%= params[field] || 'http://' %>" /></dd>
+  </dl>
+  <% end %>
+  <p><input type="submit" value="Get the Output!" /></p>
+</form>
+
+<% unless @errors.empty? %>
+<div id="errors">
+  <h2>Errors!</h2>
+  <ul>
+    <% @errors.each do |error| %>
+    <li><%= error %></li>
+    <% end %>
+  </ul>
+</div>
+<% end %>
+
+<% if @out %>
+<pre id="out"><code><%= @out %></code></pre>
+<% end %>
+
