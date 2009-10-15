3bc1bde3c1d0858be48d8108d9f627f4a55f6133
Matt Puchlerz <matt@puchlerz.com>
2009-10-15 08:11:43 -0500

    Renamed some methods. Added some loading styles.

--- public/autopilot.js
+++ public/autopilot.js
@@ -5,8 +5,12 @@ var Autopilot = new Class({
   checkbox: null,
   form: null,
   formTimer: null,
+  label: null,
   
   options: {
+    classes: {
+      loading: 'loading'
+    },
     delay: 2000,
     update: null
   },
@@ -14,35 +18,46 @@ var Autopilot = new Class({
   initialize: function(form, options) {
     this.setOptions(options);
     this.setupForm(form);
-    this.createCheckbox();
-    this.checkbox.addEvent('change', this.checkboxEvent.bindWithEvent(this));
+    this.createElements();
+    this.checkbox.addEvent('change', this.toggleFormAutopilot.bindWithEvent(this));
     Autopilot.instances++;
   },
   
-  checkboxEvent: function(event) {
-    if ( this.checkbox.get('checked') )
-      this.formTimer = this.form.send.periodical(this.options.delay, this.form);
-    else
-      $clear(this.formTimer);
-  },
-  
-  createCheckbox: function() {
-    var label = new Element('label', {
+  createElements: function() {
+    this.label = new Element('label', {
       'class': 'autopilot', 
       'for':   'autopilot' + Autopilot.instances,
       'html':  '<input id="autopilot' + Autopilot.instances + '" type="checkbox" name="autopilot" /> Autopilot'
     });
-    label.inject(this.form);
-    this.checkbox = label.getElement('input');
+    this.label.inject(this.form);
+    this.checkbox = this.label.getElement('input');
+  },
+  
+  hideLoader: function() {
+    this.form.removeClass(this.options.classes.loading);
   },
   
   setupForm: function(form) {
     this.form = $(form);
     var newRequestInstance = new Request.HTML({ 
-      'update': this.options.update,
-      'url':    this.form.get('action') 
+      'onComplete': this.hideLoader.bind(this) ,
+      'onRequest':  this.showLoader.bind(this),
+      'update':     this.options.update,
+      'url':        this.form.get('action')
     });
     this.form.store('send', newRequestInstance);
+  },
+  
+  showLoader: function() {
+    this.form.addClass(this.options.classes.loading);
+  },
+  
+  toggleFormAutopilot: function() {
+    if ( this.checkbox.get('checked') )
+      this.formTimer = this.form.send.periodical(this.options.delay, this.form);
+    else
+      $clear(this.formTimer);
+    this.checkbox.blur();
   }
   
 });
